11aca85dbf84270b1ef142f365bd405937240130
tavishac <tavishachandana29@gmail.com>
2026-03-13 13:12:38 +0530

    Fix API deployment

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -1,46 +1,35 @@
-const db = require("../config/database");
 const calculateDistance = require("../utils/distanceCalculator");
 
-// Add School API
+let schools = [];
+
 exports.addSchool = (req, res) => {
 
   const { name, address, latitude, longitude } = req.body;
 
-  // Validate input
   if (!name || !address || !latitude || !longitude) {
     return res.status(400).json({
       message: "All fields are required"
     });
   }
 
-  const query =
-    "INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)";
+  const newSchool = {
+    id: schools.length + 1,
+    name,
+    address,
+    latitude,
+    longitude
+  };
 
-  db.query(query, [name, address, latitude, longitude], (err, result) => {
+  schools.push(newSchool);
 
-    // SHOW FULL DATABASE ERROR
-    if (err) {
-      console.error("Database Error:", err);
-
-      return res.status(500).json({
-        message: "Database error",
-        error: err.message,
-        code: err.code
-      });
-    }
-
-    res.json({
-      message: "School added successfully",
-      id: result.insertId
-    });
-
+  res.json({
+    message: "School added successfully",
+    school: newSchool
   });
 
 };
 
 
-
-// List Schools API
 exports.listSchools = (req, res) => {
 
   const userLat = parseFloat(req.query.latitude);
@@ -52,39 +41,24 @@ exports.listSchools = (req, res) => {
     });
   }
 
-  db.query("SELECT * FROM schools", (err, schools) => {
+  const schoolList = schools.map((school) => {
 
-    if (err) {
-      console.error("Database Error:", err);
+    const distance = calculateDistance(
+      userLat,
+      userLon,
+      school.latitude,
+      school.longitude
+    );
 
-      return res.status(500).json({
-        message: "Database error",
-        error: err.message,
-        code: err.code
-      });
-    }
-
-    const schoolList = schools.map((school) => {
-
-      const distance = calculateDistance(
-        userLat,
-        userLon,
-        school.latitude,
-        school.longitude
-      );
-
-      return {
-        ...school,
-        distance
-      };
-
-    });
-
-    // sort by nearest distance
-    schoolList.sort((a, b) => a.distance - b.distance);
-
-    res.json(schoolList);
+    return {
+      ...school,
+      distance
+    };
 
   });
+
+  schoolList.sort((a, b) => a.distance - b.distance);
+
+  res.json(schoolList);
 
 };
